abd0c5a3419f24499a5d447ecbdbdfbac3308eda
eunseo <80867941+leeeunseo912@users.noreply.github.com>
2022-02-03 11:40:28 +0900

    pybo

---
+++ pybo/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import get_object_or_404, render, get_object_or_404, redirect
+
+from pybo.models import Question, Answer
+from django.utils import timezone
+from .forms import QuestionForm
+
+# Create your views here.
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by("-create_date")
+    total_count = Question.objects.count()
+    context = {
+        'question_list' : question_list,
+        'total_count' : total_count
+        }
+
+    return render(request, 'pybo/question_list.html', context)
+    
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question' : question}
+    return render(request, 'pybo/question_detail.html',context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    content = request.POST.get('content')
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content = content, create_date=timezone.now())
+
+    return redirect('pybo:detail', question_id = question.id)
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    
+    context = {'form':form}
+    return render(request, 'pybo/question_form.html', context)
