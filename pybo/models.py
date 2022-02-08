b757ab4586238d668e8dc3e8069bbb620f21d06d
eunseo <80867941+leeeunseo912@users.noreply.github.com>
2022-02-08 12:58:18 +0900

    3장완료

--- pybo/models.py
+++ pybo/models.py
@@ -7,6 +7,7 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField() #글 등록할 때 timezone으로부터 받아옴
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -16,6 +17,18 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
     
+    def __str__(self):
+        return self.content
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.content
